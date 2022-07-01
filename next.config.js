01e16fa9a9a87b2b514b23422cafd617785fc75c
Diego <diegojairhl@gmail.com>
2022-06-30 19:32:14 -0500

    new domain for image added

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["cdn.shopify.com", "i.ytimg.com", "loremflickr.com"],
+    domains: ["cdn.shopify.com", "i.ytimg.com", "loremflickr.com", "images.unsplash.com"],
   },
 };
 
