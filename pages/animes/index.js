23497b3fe63e4579b6cdb0666533318a9f90adb9
Valeria <vcerpasalas@gmail.com>
2022-07-02 03:41:13 -0500

    ajuste texto

--- pages/animes/index.js
+++ pages/animes/index.js
@@ -26,7 +26,9 @@ const AnimesPage = ({ data }) => {
   return (
     <Layout>
       <section className="max-w-2xl py-10 px-4 pb-16 mx-auto sm:pb-24 sm:px-6 lg:max-w-7xl lg:px-8 flex flex-col gap-6">
-        <h2 className="text-2xl text-center font-bold ">Explora tus animes favoritos</h2>
+        <h2 className="text-2xl text-center font-bold ">
+          Explora tus animes favoritos
+        </h2>
         <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {data &&
             data.map((category, index) => {
